e5c9f0a1418bf8fb83823fdd111e3128bf4aa3c7
brycelarkin <8347433+BryceLarkin@users.noreply.github.com>
2022-12-11 16:51:13 -0800

    first commit

---
+++ packages/base/src/HelpCenter.ts
@@ -0,0 +1,320 @@
+import { EventEmitter } from "eventemitter3"
+import merge from "lodash.merge"
+import {
+  DEFAULT_PARAMS,
+  ContexerLaunchError,
+  ContexerLaunchParams,
+  ContexerLaunchSessionParams,
+  ContexerLaunchStatus,
+  ContexerParams,
+} from "./config"
+import { version } from "../package.json"
+
+const MAX_LAUNCH_RETRY = 10
+
+/**
+ * ContexerImporter class manages the iframe
+ * used for importing data in your application
+ * and emits events based on what happens
+ */
+export class ContexerImporterClass extends EventEmitter {
+  #params: ContexerParams
+  iframe?: HTMLIFrameElement
+  _client = "Importer"
+  _version = version
+  _hasLaunched = false
+  _hasCancelled = false
+  static #isLoaded = false
+
+  constructor(params: ContexerParams) {
+    super()
+
+    this.#params = merge({}, DEFAULT_PARAMS, params)
+
+    if (typeof window === "undefined") {
+      return
+    }
+
+    if (this.#params.manageDOM) {
+      const iframeId = "_contexer-iframe"
+      this.iframe = document.getElementById(iframeId) as HTMLIFrameElement
+      if (this.iframe) {
+        this.iframe.dataset.count = `${parseInt(this.iframe.dataset.count || "0") + 1}`
+      } else {
+        const iframe = document.createElement("iframe")
+        iframe.id = iframeId
+        iframe.dataset.count = "1"
+        this.setIframe(iframe)
+      }
+
+      let parent = document.body
+      if (this.#params.parentId) {
+        parent = document.getElementById(this.#params.parentId) || parent
+      }
+
+      this.setParent(parent)
+    }
+  }
+
+  /**
+   * Set the name and version of the client, used for logging/debugging
+   * @param client
+   * @param version
+   */
+  setClient(client: string, version: string) {
+    this._client = client
+    this._version = version
+  }
+
+  /**
+   * Set the iframe to be used by the Contexer importer
+   * Should only be used in conjuction with the param of manageDOM false
+   * @param iframe
+   */
+  setIframe(iframe: HTMLIFrameElement) {
+    // just in case..
+    if (this.iframe) {
+      this.close(true)
+    }
+
+    this.iframe = iframe
+    this.iframe.setAttribute("allowtransparency", "true")
+
+    const queryParams = []
+    queryParams.push(`embed_client_id=${this.#params.clientId}`)
+    queryParams.push(`dev_mode=${this.#params.devMode}`)
+    if (this.#params.languageCode) {
+      queryParams.push(`lng=${this.#params.languageCode}`)
+    }
+
+    this.iframe.src = `${this.#params.baseUrl}/embed-launcher?${queryParams.join("&")}`
+    this.setClassName(this.#params.className || "")
+    if (this.#params.styles) {
+      this.setStyles(this.#params.styles)
+    }
+
+    ContexerImporterClass.#isLoaded = false
+    this.iframe.onload = () => {
+      ContexerImporterClass.#isLoaded = true
+    }
+
+    this.#hide()
+  }
+
+  /**
+   * Will change the CSS class of the iframe
+   * @param className the new CSS class
+   */
+  setClassName(className: string) {
+    if (this.iframe) {
+      this.iframe.className = className
+    }
+  }
+
+  /**
+   * Will change the styles of the iframe
+   * @param styles the styles to apply
+   */
+  setStyles(styles: Partial<CSSStyleDeclaration>) {
+    if (this.iframe) {
+      // we save display because we use it for whether
+      // the iframe is shown or not
+      const display = this.iframe.style.display
+      Object.assign(this.iframe.style, styles)
+      this.iframe.style.display = display
+    }
+  }
+
+  /**
+   * Will change the parent container of the iframe
+   * NOTE: will reload the URL
+   * @param parent DOM element to append to
+   */
+  setParent(parent: HTMLElement) {
+    if (this.iframe) {
+      parent.append(this.iframe)
+    }
+  }
+
+  /**
+   * Launch will show the Contexer window and initialize the importer session
+   * @param launchParams optionally pass in parameter overrides or values not passed into constructor
+   */
+  launch(launchParams?: Partial<ContexerLaunchParams>): ContexerLaunchStatus {
+    const mergedParams = merge({}, this.#params, launchParams)
+    const message: any = { messageType: "init" }
+    message.manualClose = true
+    message.customizationOverrides = mergedParams.customizationOverrides
+
+    message.userJwt = mergedParams.userJwt
+    if (!message.userJwt) {
+      console.error("Contexer config error: missing userJwt")
+      return { success: false, error: ContexerLaunchError.MissingJwt }
+    }
+
+    message.templateKey = mergedParams.templateKey
+    if (!message.templateKey) {
+      console.error("Contexer config error: missing templateKey")
+      return { success: false, error: ContexerLaunchError.MissingTemplate }
+    }
+
+    if (mergedParams.importConfig) {
+      message.importConfig = mergedParams.importConfig
+    }
+
+    this._launch(message)
+    return { success: true }
+  }
+
+  /**
+   * Launch session will show the Contexer window and initialize the importer session
+   * with the given session token
+   * @param launchParams optionally pass in parameter overrides or values not passed into constructor
+   */
+  launchSession(
+    launchParams?: Partial<ContexerLaunchSessionParams>,
+  ): ContexerLaunchStatus {
+    const mergedParams = merge({}, this.#params, launchParams)
+    const message: any = { messageType: "init-session" }
+    message.manualClose = true
+    message.customizationOverrides = mergedParams.customizationOverrides
+
+    message.sessionToken = mergedParams.sessionToken
+    if (!message.sessionToken) {
+      console.error("Contexer config error: missing sessionToken")
+      return { success: false, error: ContexerLaunchError.MissingSessionToken }
+    }
+
+    this._launch(message)
+    return { success: true }
+  }
+
+  _launch(message: any) {
+    window.addEventListener("message", this.#eventListener)
+
+    const postInit = () => {
+      this._hasCancelled = false
+      this._initWithRetry({
+        version: this._version,
+        client: this._client,
+        ...message,
+      })
+      ContexerImporterClass.#isLoaded = true
+    }
+
+    if (ContexerImporterClass.#isLoaded) {
+      postInit()
+    } else if (this.iframe) {
+      this.iframe.onload = postInit
+    }
+  }
+
+  _initWithRetry(message: any, count = 1) {
+    if (this._hasLaunched || this._hasCancelled) {
+      return
+    }
+
+    if (count > MAX_LAUNCH_RETRY) {
+      console.error("Contexer failed to respond for initialization")
+      if (this.#params.devMode) {
+        // there might be some error in which case,
+        // it's good to surface to devs
+        this.#show()
+      }
+
+      return
+    }
+
+    this.iframe?.contentWindow?.postMessage(message, this.#params.baseUrl || "")
+    setTimeout(() => this._initWithRetry(message, count + 1), 500)
+  }
+
+  /**
+   * Close will stop the importing session and hide the Contexer window
+   * @param clean will remove the iframe and event listeners if true
+   */
+  close(clean?: boolean) {
+    this.#hide()
+    if (this.iframe && ContexerImporterClass.#isLoaded) {
+      this.iframe.contentWindow?.postMessage(
+        { messageType: "close" },
+        this.#params.baseUrl || "",
+      )
+    }
+
+    this._hasLaunched = false
+    this._hasCancelled = true
+
+    if (clean && this.iframe) {
+      if (!this.iframe.dataset.count || this.iframe.dataset.count === "1") {
+        this.removeAllListeners()
+        window.removeEventListener("message", this.#eventListener)
+        if (this.#params.manageDOM) {
+          this.iframe.remove()
+        }
+      } else {
+        this.iframe.dataset.count = `${parseInt(this.iframe.dataset.count || "1") - 1}`
+      }
+    }
+  }
+
+  #hide() {
+    if (this.iframe) {
+      this.iframe.style.display = "none"
+    }
+  }
+
+  #show() {
+    if (this.iframe) {
+      this.iframe.style.display = "initial"
+    }
+  }
+
+  #eventListener = (event: MessageEvent) => {
+    if (event.source !== this.iframe?.contentWindow) {
+      return
+    }
+
+    switch (event.data.messageType) {
+      case "launched": {
+        this._hasLaunched = true
+        this.emit("launched")
+        this.#show()
+        break
+      }
+      case "complete": {
+        this.emit("success", event.data.data)
+        if (this.#params.autoClose) {
+          this.close()
+        }
+
+        break
+      }
+      case "cancel": {
+        this.emit("cancel")
+        if (this.#params.autoClose) {
+          this.close()
+        }
+
+        break
+      }
+      case "error": {
+        this.emit("error", event.data.message)
+        if (this.#params.autoClose) {
+          this.close()
+        }
+        break
+      }
+    }
+  }
+}
+
+/**
+ * @param params the settings for the importing session
+ * @returns an instance of the ContexerImporter
+ */
+export default function contexerHelpCenter(
+  params: ContexerParams,
+): ContexerImporterClass {
+  return new ContexerImporterClass(params)
+}
